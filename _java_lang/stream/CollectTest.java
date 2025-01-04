7e784debc9fb67bdcbc7721a7a451d03c48ea8f2
jay kim <kim.jungjin@gmail.com>
2025-01-04 13:36:01 +0900

    Update CollectTest.java

--- _java_lang/stream/CollectTest.java
+++ _java_lang/stream/CollectTest.java
@@ -22,11 +22,13 @@ public class CollectTest {
         let_me_try();
     }
 
+    //@Test
     static void let_me_try() {
-        List<String> list = null;
-        Set<String> set = null;
-        Map<String,String> map = null;
-        Map<String,List<String>> mapsl = null;
+        List<Item> list = null;
+        Set<Item> set = null;
+        Map<String,String> k2v = null;
+        Map<String,Item> k2i = null;
+        Map<String,List<Item>> k2l = null;
 
         ////////////////////////////////////////////////////////////
         // toList()
@@ -53,32 +55,32 @@ public class CollectTest {
         ////////////////////////////////////////////////////////////
         // toMap()
 
-        map = _stream()
+        k2v = _stream()
                 .collect(
                         // collector
                         toMap(
                                 // keyMapper
-                                s -> s.substring(0, 1),
+                                item -> item.k(),
                                 // valueMapper
-                                s -> s.substring(1)
+                                item -> item.v()
                         )
                 );
-        System.out.printf("%s\n", map);
+        System.out.printf("%s\n", k2v);
             //-> {Q=ux, B=ar, F=oo}
 
         //////////////////////////////
 
-        map = _stream()
+        k2i = _stream()
                 .collect(
                         // collector
                         toMap(
                                 // keyMapper
-                                s -> s.substring(0, 1),
+                                item -> item.k(),
                                 // valueMapper
                                 Function.identity()
                         )
                 );
-        System.out.printf("%s\n", map);
+        System.out.printf("%s\n", k2i);
             //-> {Q=Qux, B=Bar, F=Foo}
 
         ////////////////////////////////////////////////////////////
@@ -105,53 +107,57 @@ public class CollectTest {
         ////////////////////////////////////////////////////////////
         // groupingBy(...)
 
-        mapsl = _stream_list()
+        k2l = _stream_list()
                 .flatMap(Collection::stream)
                 .collect(
                         // collector
                         groupingBy(
                                 // classifier
-                                s -> s.substring(0, 1)
+                                Item::k
                         )
                 );
-        System.out.printf("%s\n", mapsl);
+        System.out.printf("%s\n", k2l);
             //-> {Q=[Qux, Quxa, Quxi], B=[Bar, Bard, Bark], F=[Foo, Food, Foot]}
 
         //////////////////////////////
 
-        map = _stream_list()
+        k2v = _stream_list()
                 .flatMap(Collection::stream)
                 .collect(
                         // collector
                         groupingBy(
                                 // classifier
-                                s -> s.substring(0, 1),
+                                item -> item.k(),
                                 // downstream
-                                joining(",")
+                                mapping(
+                                        // mapper
+                                        Item::toString,
+                                        // downstream
+                                        joining(","))
                         )
                 );
-        System.out.printf("%s\n", map);
+        System.out.printf("%s\n", k2v);
             //-> {Q=Qux,Quxa,Quxi, B=Bar,Bard,Bark, F=Foo,Food,Foot}
 
         //////////////////////////////
 
-        map = _stream_list()
+        k2v = _stream_list()
                 .flatMap(Collection::stream)
                 .collect(
                         // collector
                         groupingBy(
                                 // classifier
-                                s -> s.substring(0, 1),
+                                Item::k,
                                 // downstream
                                 mapping(
                                         // mapper
-                                        s -> s.substring(1),
+                                        Item::v,
                                         // downstream
                                         joining(",")
                                 )
                         )
                 );
-        System.out.printf("%s\n", map);
+        System.out.printf("%s\n", k2v);
             //-> {Q=ux,uxa,uxi, B=ar,ard,ark, F=oo,ood,oot}
 
         ////////////////////////////////////////////////////////////
@@ -172,7 +178,7 @@ public class CollectTest {
 
         //////////////////////////////
 
-        mapsl = _stream_list()
+        k2l = _stream_list()
                 .collect(
                         // collector
                         flatMapping(
@@ -181,16 +187,16 @@ public class CollectTest {
                                 // downstream
                                 groupingBy(
                                         // classifier
-                                        s -> s.substring(0, 1)
+                                        Item::k
                                 )
                         )
                 );
-        System.out.printf("%s\n", mapsl);
+        System.out.printf("%s\n", k2l);
             //-> {Q=[Qux, Quxa, Quxi], B=[Bar, Bard, Bark], F=[Foo, Food, Foot]}
 
         //////////////////////////////
 
-        map = _stream_list()
+        k2v = _stream_list()
                 .collect(
                         // collector
                         flatMapping(
@@ -199,38 +205,43 @@ public class CollectTest {
                                 // downstream
                                 groupingBy(
                                         // classifier
-                                        s -> s.substring(0, 1),
-                                        // downstream
-                                        joining(",")
-                                )
-                        )
-                );
-        System.out.printf("%s\n", map);
-            //-> {Q=Qux,Quxa,Quxi, B=Bar,Bard,Bark, F=Foo,Food,Foot}
-
-        //////////////////////////////
-
-        map = _stream_list()
-                .collect(
-                        // collector
-                        flatMapping(
-                                // mapper
-                                Collection::stream,
-                                // downstream
-                                groupingBy(
-                                        // classifier
-                                        s -> s.substring(0, 1),
+                                        Item::k,
                                         // downstream
                                         mapping(
                                                 // mapper
-                                                s -> s.substring(1),
+                                                Item::toString,
                                                 // downstream
                                                 joining(",")
                                         )
                                 )
                         )
                 );
-        System.out.printf("%s\n", map);
+        System.out.printf("%s\n", k2v);
+            //-> {Q=Qux,Quxa,Quxi, B=Bar,Bard,Bark, F=Foo,Food,Foot}
+
+        //////////////////////////////
+
+        k2v = _stream_list()
+                .collect(
+                        // collector
+                        flatMapping(
+                                // mapper
+                                Collection::stream,
+                                // downstream
+                                groupingBy(
+                                        // classifier
+                                        Item::k,
+                                        // downstream
+                                        mapping(
+                                                // mapper
+                                                Item::v,
+                                                // downstream
+                                                joining(",")
+                                        )
+                                )
+                        )
+                );
+        System.out.printf("%s\n", k2v);
             //-> {Q=ux,uxa,uxi, B=ar,ard,ark, F=oo,ood,oot}
 
         ////////////////////////////////////////////////////////////
@@ -238,6 +249,7 @@ public class CollectTest {
 
         int maxLen = 0;
         maxLen= _stream()
+                .map(Item::toString)
                 .collect(
                         reducing(
                                 0,
@@ -253,29 +265,51 @@ public class CollectTest {
         // reduce(identity, accumulator)
 
         maxLen = _stream()
+                .map(Item::toString)
                 .map(String::length)
                 .reduce(0, Integer::max);
         System.out.printf("%d\n", maxLen);
             //-> 3
     }
 
-    private static Stream<String> _stream() {
-        return Stream.of("Foo", "Bar", "Qux");
+    private static Stream<Item> _stream() {
+        return Stream.of(Item.of("Foo"), Item.of("Bar"), Item.of("Qux"));
     }
 
-    private static Stream<List<String>> _stream_list() {
+    private static Stream<List<Item>> _stream_list() {
         return Stream.of(
-                List.of("Foo", "Bar", "Qux"),
-                List.of("Food", "Bard", "Quxa"),
-                List.of("Foot", "Bark", "Quxi")
+                List.of(Item.of("Foo"), Item.of("Bar"), Item.of("Qux")),
+                List.of(Item.of("Food"), Item.of("Bard"), Item.of("Quxa")),
+                List.of(Item.of("Foot"), Item.of("Bark"), Item.of("Quxi"))
         );
     }
 
-    private static Stream<String[]> _stream_array() {
+    private static Stream<Item[]> _stream_array() {
         return Stream.of(
-                new String[] { "Foo", "Bar", "Qux" },
-                new String[] { "Food", "Bard", "Quxa" },
-                new String[] { "Foot", "Bark", "Quxi" }
+                new Item[] { Item.of("Foo"), Item.of("Bar"), Item.of("Qux") },
+                new Item[] { Item.of("Food"), Item.of("Bard"), Item.of("Quxa") },
+                new Item[] { Item.of("Foot"), Item.of("Bark"), Item.of("Quxi") }
         );
+    }
+    
+    private static class Item {
+        private final String k;
+        private final String v;
+        private Item(String s) {
+            k = s.substring(0, 1);
+            v = s.substring(1);
+        }
+        public static Item of(String s) {
+            return new Item(s);
+        }
+        public String k() {
+            return k;
+        }
+        public String v() {
+            return v;
+        }
+        public String toString() {
+            return String.format("%s%s", k, v);
+        }
     }
 }
